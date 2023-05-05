fa69bd6bfd481b641c8cc2877c4aea8c1ceb555d
RonaldDBezerra <rebulgame@gmail.com>
2023-05-05 16:58:42 -0300

    adiciona style thema para a aplicação

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import {extendTheme} from "native-base"
+
+export default THEME =  extendTheme({
+  colors: {
+    blue: {
+      500: "#3A49F9"
+    },
+    white: "#e5eafc",
+    green: {
+      300: "#6FEA8B"
+    }
+  }
+})
